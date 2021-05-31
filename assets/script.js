a0bc777e33a69f397b02ba8e78d01fb93d121e27
Kalyn Reid <reidkalyn361@gmail.com>
2021-05-30 21:22:19 -0700

    Altered border radius. Experimenting with question bank. Moved inputs to the HTML file.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//Get elements from the DOM.
 const startButton = document.getElementById("start-button");
 const header = document.getElementById("header");
 const description = document.getElementById("description");
@@ -5,13 +6,24 @@ const questionContainer = document.getElementById("question-container");
 const questionText = document.getElementById("question-text");
 const answerContainer = document.getElementById("answer-container");
 const timer = document.getElementById("timer");
-let answerListEl = document.createElement("li");
-let answerButtonEl = document.createElement("button")
 const highscoreContainer = document.getElementById("highscore-container");
 const highscoreList = document.getElementById("highscore-list");
+const submitEl = document.getElementById("submit-button");
+const initialsEl = document.getElementById("initials-input");
 
+//Create list element for answers.
+let answerListEl = document.createElement("li");
+//Create button element for each answer in the answer list. 
+let answerButtonEl = document.createElement("button");
+//Create list element for highscore.
+let highscoreRecord = document.createElement("li");
+
+//Hide Highscore Container until game over.
+highscoreContainer.setAttribute("style","display:none");
+
+//Allow timer to count down and changes text color to red when <5sec remain. 
 function handleTimer(){
-    let sec = 30;
+    let sec = 60; //CHANGE BACK TO APPROPRIATE TIME WHEN DONE TESTING!
     let time = setInterval(function(){
         timer.textContent="Timer: " + sec + " seconds left.";
         sec--;
@@ -20,12 +32,12 @@ function handleTimer(){
         }
         if (sec < 0) {
             clearInterval(time);
-            console.log("Out of Time!");
-            handleGameOver(questionContainer)
+            handleGameOver();
         }
     }, 1000);
 }
 
+//Create a bank of questions and answers to pull from.
 const questionBank = [
     {question: "Which of the following is NOT a data type?",
     answers: ["Boolean","Number","Date","String"],
@@ -44,80 +56,98 @@ const questionBank = [
     correctAnswer:"click"}
 ];
 
-questionContainer.setAttribute("style","visibility:hidden")
+//Hide question container until the game starts.
+questionContainer.setAttribute("style","display:none");
 
+//Create first question on start. 
+//Show question container on start. 
 function handleStartGame(event) {
-    console.log(startButton.textContent);
     startButton.remove();
-    description.textContent = ("Game Start! First Question:")
-    questionContainer.setAttribute("style","")
-    questionText.textContent = (questionBank[0].question)
+    description.textContent = ("Game Start! First Question:");
+    questionContainer.setAttribute("style","");
+    handleNextQuestion(0)
+}
+
+function handleNextQuestion (number) {
+    questionText.textContent = (questionBank[number].question);
     for (let i = 0; i < 4; i++) {
         answerListEl = document.createElement("li");
         answerButtonEl = document.createElement("button")
-        answerButtonEl.textContent = (questionBank[0].answers[i]);
+        answerButtonEl.textContent = (questionBank[number].answers[i]);
         answerContainer.append(answerListEl);
         answerListEl.append(answerButtonEl)
     }
-}
-
-function handleNextQuestion() {
-    for (let int = 0; int < questionBank.length; int++) {
-        questionText.textContent.remove()
-        answerListEl.remove()
-        questionText.textContent = (questionBank[int].question)
+    let userAnswer = event.target.textContent
+    if (userAnswer===questionBank[number].correctAnswer){
+        description.textContent = ("Next Question:")
+        console.log("Correct")
+        number++
+        questionText.textContent = (questionBank[number].question)
         for (let i = 0; i < 4; i++) {
             answerListEl = document.createElement("li");
             answerButtonEl = document.createElement("button")
-            answerButtonEl.textContent = (questionBank[int].answers[i]);
+            answerButtonEl.textContent = (questionBank[number].answers[i]);
+            answerContainer.append(answerListEl);
+            answerListEl.append(answerButtonEl)
+        }
+    } else {
+        description.textContent = ("Next Question:")
+        console.log("Wrong")
+        number++
+        questionText.textContent = (questionBank[number].question)
+        for (let i = 0; i < 4; i++) {
+            answerListEl = document.createElement("li");
+            answerButtonEl = document.createElement("button")
+            answerButtonEl.textContent = (questionBank[number].answers[i]);
             answerContainer.append(answerListEl);
             answerListEl.append(answerButtonEl)
         }
     }
-    return
-}
-
-
     
-
-function handleWrongAnswer() {
-    description.textContent = ("Wrong! Next Question:")
-    console.log("Wrong")
-}
-
-function handleGameOver(element) {
-    element.remove();
-    description.textContent = ("Game Over!");
-    let highscoreRecord = document.createElement("li");
-    let highscoreNumber = sec
-    highscoreContainer.setAttribute("style","");
-    const initialsEl = document.createElement("input");
-    initialsEl.setAttribute("type","text");
-    initialsEl.setAttribute("name","Initials");
-    initialsEl.setAttribute("placeholder","Type your initials here.");
-    highscoreContainer.append(initialsEl);
-    const submitEl = document.createElement("input");
-    submitEl.setAttribute("type","submit");
-    submitEl.setAttribute("value","Submit");
-    submitEl.setAttribute("id","submit");
-    highscoreContainer.append(submitEl);
-
+    if (number>questionBank.length){
+        handleGameOver()
+    }
 
 }
-
-function handleSubmit (){
-    let highscoreRecord = document.createElement("li");
-    highscoreRecord.textContent=(initialsEl.value + ": " + sec)
-}
-
-startButton.addEventListener("click", handleStartGame);
-startButton.addEventListener("click", handleTimer);
-
-submitEl.addEventListener("click", handleSubmit);
 
 answerButtonEl.addEventListener("click", handleNextQuestion);
 
+function handleGameOver() {
+    questionContainer.setAttribute("style","display:none")
+    description.textContent = ("Game Over!");
+    highscoreContainer.setAttribute("style","");
+}
 
+function handleSubmit (event){
+    event.preventDefault();
+    let highscoreAr = []
+    let timeLeft = ""
+    if (timer.textContent.length===22){
+        timeLeft = parseInt(timer.textContent.substr(7,1));
+        highscoreAr.push(timeLeft);
+    } else if (timer.textContent.length===23){
+        timeLeft = parseInt(timer.textContent.substr(7,2));
+        highscoreAr.push(timeLeft);
+    } else if (timer.textContent.length===24){
+        timeLeft = parseInt(timer.textContent.substr(7,3));
+        highscoreAr.push(timeLeft);
+    }
+    highscoreAr.sort()
+    console.log(highscoreAr)
+    console.log(highscoreRecord)
+    for (let i = 0; i < highscoreAr.length; i++) {
+        highscoreRecord = document.createElement("li");
+        highscoreRecord.textContent=(initialsEl.value + ": " + highscoreAr[i]);
+        highscoreList.append(highscoreRecord);
+    }
+}
+
+//Have the start button start the timer and start the quiz on click. 
+startButton.addEventListener("click", handleStartGame);
+startButton.addEventListener("click", handleTimer);
+
+
+submitEl.addEventListener("click", handleSubmit);
 
 
 
